8ad71265d1a06d314fbe801d239b668f56e861ee
corsacca <corsacca@gmail.com>
2019-04-17 11:19:06 +0100

    Fix plugins calling undefined function when theme not active.

--- disciple-tools-mobile-app-plugin.php
+++ disciple-tools-mobile-app-plugin.php
@@ -45,7 +45,7 @@ function dt_mobile_app() {
      * Load useful function from the theme
      */
     if ( !defined( 'DT_FUNCTIONS_READY' ) ){
-        require get_template_directory() . '/dt-core/global-functions.php';
+        require_once get_template_directory() . '/dt-core/global-functions.php';
     }
     /*
      * Don't load the plugin on every rest request. Only those with the metrics namespace
@@ -299,7 +299,38 @@ function dt_mobile_app_hook_admin_notice() {
     if ( $wp_theme->get_template() === "disciple-tools-theme" ){
         $message .= sprintf( esc_html__( 'Current Disciple Tools version: %1$s, required version: %2$s', 'dt_mobile' ), esc_html( $current_version ), esc_html( $dt_mobile_app_required_dt_theme_version ) );
     }
-    dt_hook_admin_notice( $message, 'dt-mobile-app' );
+    // Check if it's been dismissed...
+    if ( ! get_option( 'dismissed-dt-mobile-app', false ) ) { ?>
+        <div class="notice notice-error notice-dt-mobile-app is-dismissible" data-notice="dt-mobile-app">
+            <p><?php echo esc_html( $message );?></p>
+        </div>
+        <script>
+            jQuery(function($) {
+                $( document ).on( 'click', '.notice-dt-mobile-app .notice-dismiss', function () {
+                    $.ajax( ajaxurl, {
+                        type: 'POST',
+                        data: {
+                            action: 'dismissed_notice_handler',
+                            type: 'dt-mobile-app',
+                            security: '<?php echo esc_html( wp_create_nonce( 'wp_rest_dismiss' ) ) ?>'
+                        }
+                    })
+                });
+            });
+        </script>
+    <?php }
+}
+/**
+ * AJAX handler to store the state of dismissible notices.
+ */
+if ( !function_exists( "dt_hook_ajax_notice_handler" )){
+    function dt_hook_ajax_notice_handler(){
+        check_ajax_referer( 'wp_rest_dismiss', 'security' );
+        if ( isset( $_POST["type"] ) ){
+            $type = sanitize_text_field( wp_unslash( $_POST["type"] ) );
+            update_option( 'dismissed-' . $type, true );
+        }
+    }
 }
 
 add_action( "plugin_loaded", function(){
